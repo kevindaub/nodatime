f3d2e5f9964864666405904ed7c5eca414ce0e15
James Keesey <James Keesey@localhost>
2009-11-20 10:30:37 -0800

    Renamed Clock.TheClock to Clock.Current as per suggestion in code review.

---
+++ src/NodaTime/Clock.cs
@@ -0,0 +1,58 @@
+﻿#region Copyright and license information
+// Copyright 2001-2009 Stephen Colebourne
+// Copyright 2009 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using NodaTime.Clocks;
+
+namespace NodaTime
+{
+    /// <summary>
+    /// Represents a clock which can tell the current time, expressed in ticks since the Unix Epoch.
+    /// </summary>
+    /// <remarks>
+    /// A tick is the smallest time increment that the system can support. The <see
+    /// cref="DateTimeConstants.TicksPerMillisecond"/> const determines the resolution of this
+    /// clock.
+    /// </remarks>
+    public static class Clock
+    {
+        private static IClock current = SystemClock.Instance;
+
+        /// <summary>
+        /// Gets or sets the object that reports the current time. Replaceable for easier testing.
+        /// </summary>
+        /// <value>The clock object.</value>
+        public static IClock Current
+        {
+            get { return current; }
+            set
+            {
+                if (value == null) {
+                    current = SystemClock.Instance;
+                }
+                else {
+                    Current = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the current time as the number of ticks since the Unix Epoch.
+        /// </summary>
+        /// <value>The current time in ticks as an Instant.</value>
+        public static Instant Now { get { return Current.Now; } }
+    }
+}
---
+++ src/NodaTime/Periods/PeriodType.cs
@@ -0,0 +1,61 @@
+﻿#region Copyright and license information
+// Copyright 2001-2009 Stephen Colebourne
+// Copyright 2009 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+
+namespace NodaTime
+{
+    /// <summary>
+    /// Original name: PeriodType
+    /// </summary>
+    /// <remarks>
+    /// TODO: Consider where we should really have ticks. It's not entirely clear.
+    /// Defined values are:
+    /// 
+    /// Standard - years, months, weeks, days, hours, minutes, seconds, millis
+    /// YearMonthDayTime - years, months, days, hours, minutes, seconds, millis
+    /// YearMonthDay - years, months, days
+    /// YearWeekDayTime - years, weeks, days, hours, minutes, seconds, millis
+    /// YearWeekDay - years, weeks, days
+    /// YearDayTime - years, days, hours, minutes, seconds, millis
+    /// YearDay - years, days, hours
+    /// DayTime - days, hours, minutes, seconds, millis
+    /// Time - hours, minutes, seconds, millis
+    /// plus one for each single type
+    /// </remarks>
+    public enum PeriodType
+    {
+        Years = DurationFieldType.Years,
+        Months = DurationFieldType.Months,
+        Weeks = DurationFieldType.Weeks,
+        Days = DurationFieldType.Days,
+        Hours = DurationFieldType.Hours,
+        Minutes = DurationFieldType.Minutes,
+        Seconds = DurationFieldType.Seconds,
+        Milliseconds = DurationFieldType.Milliseconds,
+
+        Standard = Years | Months | Weeks | Days | Hours | Minutes | Seconds | Milliseconds,
+        YearMonthDayTime = Years | Months | Days | Hours | Minutes | Seconds | Milliseconds,
+        YearMonthDay = Years | Months | Days,
+        YearWeekDayTime = Years | Weeks | Days | Hours | Minutes | Seconds | Milliseconds,
+        YearWeekDay = Years | Weeks | Days,
+        YearDayTime = Years | Days | Hours | Minutes | Seconds | Milliseconds,
+        YearDay = Years | Days | Hours,
+        DayTime = Days | Hours | Minutes | Seconds | Milliseconds,
+        Time = Hours | Minutes | Seconds | Milliseconds,
+    }
+}
