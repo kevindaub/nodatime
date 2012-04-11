f0d884d66c48d001c963f028eba81c673768ce6a
Jon Skeet <skeet@pobox.com>
2012-04-11 21:34:13 +0100

    Updated nuget version numbers, ready for 1.0.0-beta1 build.

---
+++ src/NodaTime.Test/DateTimeZoneTest.Ids.cs
@@ -0,0 +1,126 @@
+﻿#region Copyright and license information
+// Copyright 2001-2009 Stephen Colebourne
+// Copyright 2009-2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using NodaTime.TimeZones;
+
+namespace NodaTime.Test
+{
+    /// <summary>
+    /// Tests for code in DateTimeZone and code which will be moving out
+    /// of DateTimeZones into DateTimeZone over time.
+    /// </summary>
+    [TestFixture]
+    public partial class DateTimeZoneTest
+    {
+        [Test(Description = "Test for issue 7 in bug tracker")]
+        public void IterateOverIds()
+        {
+            // According to bug, this would go bang
+            int count = DateTimeZone.Ids.Count();
+
+            Assert.IsTrue(count > 1);
+            int utcCount = DateTimeZone.Ids.Count(id => id == DateTimeZone.UtcId);
+            Assert.AreEqual(1, utcCount);
+        }
+
+        [Test]
+        public void UtcIsNotNull()
+        {
+            Assert.IsNotNull(DateTimeZone.Utc);
+        }
+
+        [Test]
+        public void ForId_nullId()
+        {
+            Assert.Throws<ArgumentNullException>(() => DateTimeZone.ForId(null));
+        }
+
+        [Test]
+        public void ForId_UtcId()
+        {
+            Assert.AreEqual(DateTimeZone.Utc, DateTimeZone.ForId(DateTimeZone.UtcId));
+        }
+
+        [Test]
+        public void ForId_InvalidId()
+        {
+            Assert.Throws<TimeZoneNotFoundException>(() => DateTimeZone.ForId("not a known id"));
+        }
+
+        [Test]
+        public void ForId_AmericaLosAngeles()
+        {
+            const string americaLosAngeles = "America/Los_Angeles";
+            var actual = DateTimeZone.ForId(americaLosAngeles);
+            Assert.IsNotNull(actual);
+            Assert.AreNotEqual(DateTimeZone.Utc, actual);
+            Assert.AreEqual(americaLosAngeles, actual.Id);
+        }
+
+        [Test]
+        public void Ids_All()
+        {
+            var actual = DateTimeZone.Ids;
+            var actualCount = actual.Count();
+            Assert.IsTrue(actualCount > 1);
+            var utc = actual.Single(id => id == DateTimeZone.UtcId);
+            Assert.AreEqual(DateTimeZone.UtcId, utc);
+        }
+
+        /// <summary>
+        /// Simply tests that every ID in the built-in database can be fetched. This is also
+        /// helpful for diagnostic debugging when we want to check that some potential
+        /// invariant holds for all time zones...
+        /// </summary>
+        [Test]
+        public void ForId_AllIds()
+        {
+            foreach (string id in DateTimeZone.Ids)
+            {
+                Assert.IsNotNull(DateTimeZone.ForId(id));
+            }
+        }
+
+        [Test]
+        public void For_Id_FixedOffset()
+        {
+            string id = "UTC+05:30";
+            DateTimeZone zone = DateTimeZone.ForId(id);
+            Assert.AreEqual(DateTimeZone.ForOffset(Offset.FromHoursAndMinutes(5, 30)), zone);
+            Assert.AreEqual(id, zone.Id);
+        }
+
+        [Test]
+        public void For_Id_FixedOffset_NonCanonicalId()
+        {
+            string id = "UTC+05:00:00";
+            DateTimeZone zone = DateTimeZone.ForId(id);
+            Assert.AreEqual(zone, DateTimeZone.ForOffset(Offset.FromHours(5)));
+            Assert.AreEqual("UTC+05", zone.Id);
+        }
+
+        [Test]
+        public void For_Id_InvalidFixedOffset()
+        {
+            Assert.Throws<TimeZoneNotFoundException>(() => DateTimeZone.ForId("UTC+5Months"));
+        }
+    }
+}
---
+++ src/NodaTime.Test/PeriodBuilderTest.cs
@@ -0,0 +1,136 @@
+﻿#region Copyright and license information
+// Copyright 2001-2009 Stephen Colebourne
+// Copyright 2009-2012 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace NodaTime.Test
+{
+    [TestFixture]
+    public class PeriodBuilderTest
+    {
+        [Test]
+        public void Indexer_Getter_ValidUnits()
+        {
+            var builder = new PeriodBuilder
+            {
+                Years = null,
+                Months = 1,
+                Weeks = 2,
+                Days = 3,
+                Hours = 4,
+                Minutes = 5,
+                Seconds = 6,
+                Milliseconds = 7,
+                Ticks = 8
+            };
+            Assert.IsFalse(builder[PeriodUnits.Years].HasValue);
+            Assert.AreEqual(1L, builder[PeriodUnits.Months]);
+            Assert.AreEqual(2L, builder[PeriodUnits.Weeks]);
+            Assert.AreEqual(3L, builder[PeriodUnits.Days]);
+            Assert.AreEqual(4L, builder[PeriodUnits.Hours]);
+            Assert.AreEqual(5L, builder[PeriodUnits.Minutes]);
+            Assert.AreEqual(6L, builder[PeriodUnits.Seconds]);
+            Assert.AreEqual(7L, builder[PeriodUnits.Milliseconds]);
+            Assert.AreEqual(8L, builder[PeriodUnits.Ticks]);
+        }
+
+        [Test]
+        public void Index_Getter_InvalidUnits()
+        {
+            var builder = new PeriodBuilder();
+            Assert.Throws<ArgumentOutOfRangeException>(() => builder[0].GetValueOrDefault());
+            Assert.Throws<ArgumentOutOfRangeException>(() => builder[(PeriodUnits) (-1)].GetValueOrDefault());
+            Assert.Throws<ArgumentOutOfRangeException>(() => builder[PeriodUnits.DateAndTime].GetValueOrDefault());
+        }
+
+        [Test]
+        public void Indexer_Setter_ValidUnits()
+        {
+            var builder = new PeriodBuilder();
+            builder[PeriodUnits.Years] = null;
+            builder[PeriodUnits.Months] = 1;
+            builder[PeriodUnits.Weeks] = 2;
+            builder[PeriodUnits.Days] = 3;
+            builder[PeriodUnits.Hours] = 4;
+            builder[PeriodUnits.Minutes] = 5;
+            builder[PeriodUnits.Seconds] = 6;
+            builder[PeriodUnits.Milliseconds] = 7;
+            builder[PeriodUnits.Ticks] = 8;
+            var expected = new PeriodBuilder
+            {
+                Years = null,
+                Months = 1,
+                Weeks = 2,
+                Days = 3,
+                Hours = 4,
+                Minutes = 5,
+                Seconds = 6,
+                Milliseconds = 7,
+                Ticks = 8
+            };
+            Assert.AreEqual(expected, builder);
+        }
+
+        [Test]
+        public void Index_Setter_InvalidUnits()
+        {
+            var builder = new PeriodBuilder();
+            Assert.Throws<ArgumentOutOfRangeException>(() => builder[0] = 1L);
+            Assert.Throws<ArgumentOutOfRangeException>(() => builder[(PeriodUnits)(-1)] = 1L);
+            Assert.Throws<ArgumentOutOfRangeException>(() => builder[PeriodUnits.DateAndTime] = 1L);
+        }
+
+        [Test]
+        public void Build_SingleUnit()
+        {
+            Period period = new PeriodBuilder { Hours = 10 }.Build();
+
+            Period expected = Period.FromHours(10);
+            Assert.AreEqual(expected, period);
+            Assert.AreEqual(expected.Units, period.Units);
+        }
+
+        [Test]
+        public void Build_MultipleUnits()
+        {
+            Period period = new PeriodBuilder { Days = 5, Minutes = -10 }.Build();
+
+            Period expected = Period.FromDays(5) + Period.FromMinutes(-10);
+            Assert.AreEqual(expected, period);
+            Assert.AreEqual(expected.Units, period.Units);
+        }
+
+        [Test]
+        public void Build_Empty()
+        {
+            Assert.AreEqual(PeriodUnits.None, new PeriodBuilder().Build().Units);
+        }
+
+        [Test]
+        public void Equality()
+        {
+            PeriodBuilder equal1 = new PeriodBuilder { Hours = 10, Minutes = 1 };
+            PeriodBuilder equal2 = new PeriodBuilder { Minutes = 1, Hours = 10, Ticks = null };
+            PeriodBuilder unequal = new PeriodBuilder { Minutes = 1 };
+            TestHelper.TestEqualsClass(equal1, equal2, unequal);
+        }
+    }
+}
