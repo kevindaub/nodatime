e5dbb9f33a0ec292e638860e36e82e27aea19f1c
Malcolm Rowe <malcolm.rowe@gmail.com>
2012-12-15 10:04:40 +0000

    Update version history to account for recent changes.

---
+++ src/NodaTime.Test/Text/PatternTestBase.cs
@@ -0,0 +1,93 @@
+﻿#region Copyright and license information
+// Copyright 2001-2009 Stephen Colebourne
+// Copyright 2009-2010 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using NUnit.Framework;
+using NodaTime.Calendars;
+
+namespace NodaTime.Test.Text
+{
+    /// <summary>
+    /// Base class for all the pattern tests (when we've migrated OffsetPattern off FormattingTestSupport).
+    /// Derived classes should have internal static fields with the names listed in the TestCaseSource
+    /// attributes here: InvalidPatternData, ParseFailureData, ParseData, FormatData. Any field
+    /// which is missing cause that test to be "not runnable" for that concrete subclass.
+    /// </summary>
+    public abstract class PatternTestBase<T>
+    {
+        [Test]
+        [TestCaseSource("InvalidPatternData")]
+        public void InvalidPatterns(PatternTestData<T> data)
+        {
+            data.TestInvalidPattern();
+        }
+
+        [Test]
+        [TestCaseSource("ParseFailureData")]
+        public void ParseFailures(PatternTestData<T> data)
+        {
+            data.TestParseFailure();
+        }
+
+        [Test]
+        [TestCaseSource("ParseData")]
+        public void Parse(PatternTestData<T> data)
+        {
+            data.TestParse();
+        }
+
+        [Test]
+        [TestCaseSource("FormatData")]
+        public void Format(PatternTestData<T> data)
+        {
+            data.TestFormat();
+        }
+
+        /// <summary>
+        /// Tries to work out a roughly-matching calendar system for the given BCL calendar.
+        /// This is needed where we're testing whether days of the week match - even if we can
+        /// get day/month/year values to match without getting the calendar right, the calendar
+        /// affects the day of week.
+        /// </summary>
+        internal static CalendarSystem CalendarSystemForCalendar(Calendar bcl)
+        {
+            if (bcl is GregorianCalendar)
+            {
+                return CalendarSystem.Iso;
+            }
+            if (bcl is HijriCalendar)
+            {
+                return CalendarSystem.GetIslamicCalendar(IslamicLeapYearPattern.Base16, IslamicEpoch.Astronomical);
+            }
+            // This is *not* a general rule. Noda Time simply doesn't support this calendar, which requires
+            // table-based data. However, using the Islamic calendar with the civil epoch gives the same date for
+            // our sample, which is good enough for now...
+            if (bcl is UmAlQuraCalendar)
+            {
+                return CalendarSystem.GetIslamicCalendar(IslamicLeapYearPattern.Base16, IslamicEpoch.Civil);
+            }
+            if (bcl is JulianCalendar)
+            {
+                return CalendarSystem.GetJulianCalendar(4);
+            }
+            // No idea - we can't test with this calendar...
+            return null;
+        }
+    }
+}
---
+++ src/NodaTime/DateTimeZoneProviders.cs
@@ -0,0 +1,33 @@
+﻿using NodaTime.TimeZones;
+
+namespace NodaTime
+{
+    /// <summary>
+    /// Static access to date/time zone providers built into Noda Time. These are all thread-safe and caching.
+    /// </summary>
+    public static class DateTimeZoneProviders
+    {
+        private static readonly DateTimeZoneCache tzdbFactory = new DateTimeZoneCache(TzdbDateTimeZoneSource.Default);
+        private static readonly DateTimeZoneCache bclFactory = new DateTimeZoneCache(new BclDateTimeZoneSource());
+
+        /// <summary>
+        /// Gets the default time zone provider, which is initialized from resources within the NodaTime assembly.
+        /// </summary>
+        /// <remarks>
+        /// Currently this returns the same value as the <see cref="Tzdb"/> property.
+        /// </remarks>
+        public static IDateTimeZoneProvider Default { get { return Tzdb; } }
+
+        /// <summary>
+        /// Gets a time zone provider which uses a <see cref="TzdbDateTimeZoneSource"/>.
+        /// The underlying source is <see cref="TzdbDateTimeZoneSource.Default"/>, which is initialized from
+        /// resources within the NodaTime assembly.
+        /// </summary>
+        public static IDateTimeZoneProvider Tzdb { get { return tzdbFactory; } }
+
+        /// <summary>
+        /// Gets a time zone provider which uses a <see cref="BclDateTimeZoneSource"/>.
+        /// </summary>
+        public static IDateTimeZoneProvider Bcl { get { return bclFactory; } }
+    }
+}
