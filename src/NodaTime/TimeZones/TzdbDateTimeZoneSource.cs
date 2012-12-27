eb5dc8c39d888e9cee74a92634c4ed9568b50442
Jon Skeet <skeet@pobox.com>
2012-12-27 09:13:12 +0000

    Move all of the compression code from DateTimeZoneCompressionWriter/Reader into just DateTimeZoneWriter/Reader, and then remove the inheritance. We were only ever using the compressed implementation, and removing the inheritance part simplifies things. We can add it back in if we ever need it, but I'm hoping we won't...

---
+++ src/NodaTime/TimeZones/TzdbDateTimeZoneSource.cs
@@ -0,0 +1,227 @@
+#region Copyright and license information
+// Copyright 2001-2009 Stephen Colebourne
+// Copyright 2009-2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using NodaTime.Utility;
+
+namespace NodaTime.TimeZones
+{
+    /// <summary>
+    /// Provides an implementation of a <see cref="IDateTimeZoneSource" /> that looks
+    /// for its time zone definitions from a named resource in an assembly.
+    /// </summary>
+    /// <remarks>
+    /// All calls to <see cref="ForId"/> for fixed-offset IDs advertised by the source (i.e. "UTC" and "UTC+/-Offset")
+    /// will return zones equal to those returned by <see cref="DateTimeZone.ForOffset"/>.
+    /// </remarks>
+    /// <threadsafety>This type is immutable reference type. See the thread safety section of the user guide for more information.</threadsafety>
+    public sealed class TzdbDateTimeZoneSource : IDateTimeZoneSource
+    {
+        /// <summary>
+        /// A source initialised with the built-in version of TZDB.
+        /// </summary>
+        private static readonly TzdbDateTimeZoneSource builtin = new TzdbDateTimeZoneSource("NodaTime.TimeZones.Tzdb");
+
+        /// <summary>
+        /// The resource key for the Windows to TZDB ID mapping dictionary.
+        /// This resource key contains hyphens, so cannot conflict with a time zone name.
+        /// </summary>
+        internal const string WindowsToPosixMapKey = "--meta-WindowsToPosix";
+
+        /// <summary>
+        /// The resource key for the Windows to TZDB ID mapping version string.
+        /// This resource key contains hyphens, so cannot conflict with a time zone name.
+        /// </summary>
+        internal const string WindowsToPosixMapVersionKey = "--meta-WindowsToPosixVersion";
+
+        /// <summary>
+        /// The resource key for the timezone ID alias dictionary.
+        /// This resource key contains hyphens, so cannot conflict with a time zone name.
+        /// </summary>
+        internal const string IdMapKey = "--meta-IdMap";
+
+        /// <summary>
+        /// The resource key for the TZDB version ID.
+        /// This resource key contains hyphens, so cannot conflict with a time zone name.
+        /// </summary>
+        internal const string VersionKey = "--meta-VersionId";
+
+        private readonly IResourceSet source;
+
+        /// <summary>
+        /// Map from ID (possibly an alias) to canonical ID.
+        /// </summary>
+        private readonly IDictionary<string, string> timeZoneIdMap;
+
+        private readonly IDictionary<string, string> windowsIdMap;
+        private readonly ILookup<string, string> aliases;
+        private readonly string version;
+
+        /// <summary>
+        /// The <see cref="TzdbDateTimeZoneSource"/> initialised from resources within the NodaTime assembly.
+        /// </summary>
+        public static TzdbDateTimeZoneSource Default { get { return builtin; } }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TzdbDateTimeZoneSource" /> class from a resource within
+        /// the NodaTime assembly.
+        /// </summary>
+        /// <param name="baseName">The root name of the resource file.</param>
+        public TzdbDateTimeZoneSource(string baseName)
+            : this(baseName, Assembly.GetExecutingAssembly())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TzdbDateTimeZoneSource" /> class.
+        /// </summary>
+        /// <param name="baseName">The root name of the resource file.</param>
+        /// <param name="assembly">The assembly to search for the time zone resources.</param>
+        public TzdbDateTimeZoneSource(string baseName, Assembly assembly)
+            : this(ResourceHelper.GetDefaultResourceSet(baseName, assembly))
+        {
+        }
+
+#if !PCL
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TzdbDateTimeZoneSource" /> class.
+        /// </summary>
+        /// <param name="source">The <see cref="ResourceManager"/> to search for the time zone resources.</param>
+        public TzdbDateTimeZoneSource(ResourceManager source)
+            : this(ResourceHelper.GetDefaultResourceSet(source))
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TzdbDateTimeZoneSource" /> class.
+        /// </summary>
+        /// <param name="source">The <see cref="ResourceSet"/> to search for the time zone resources.</param>
+        public TzdbDateTimeZoneSource(ResourceSet source) : this(new BclResourceSet(Preconditions.CheckNotNull(source, "source")))
+        {
+        }
+#endif
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TzdbDateTimeZoneSource" /> class.
+        /// </summary>
+        /// <param name="source">The <see cref="IResourceSet"/> to search for the time zone resources.</param>
+        internal TzdbDateTimeZoneSource(IResourceSet source)
+        {
+            this.source = source;
+            var mutableIdMap = ResourceHelper.LoadDictionary(source, IdMapKey);
+            if (mutableIdMap == null)
+            {
+#if PCL
+                throw new IOException("No map with key " + IdMapKey + " in resource");
+#else
+                throw new InvalidDataException("No map with key " + IdMapKey + " in resource");
+#endif
+            }
+            timeZoneIdMap = new NodaReadOnlyDictionary<string, string>(mutableIdMap);
+            aliases = timeZoneIdMap
+                .Where(pair => pair.Key != pair.Value)
+                .OrderBy(pair => pair.Key, StringComparer.Ordinal)
+                .ToLookup(pair => pair.Value, pair => pair.Key);
+            windowsIdMap = ResourceHelper.LoadDictionary(source, WindowsToPosixMapKey);
+            if (windowsIdMap == null)
+            {
+#if PCL
+                throw new IOException("No map with key " + WindowsToPosixMapKey + " in resource");
+#else
+                throw new InvalidDataException("No map with key " + WindowsToPosixMapKey + " in resource");
+#endif
+            }
+            this.version = source.GetString(VersionKey) + " (mapping: " + source.GetString(WindowsToPosixMapVersionKey) + ")";
+        }
+
+        /// <summary>
+        /// Returns the time zone definition associated with the given id.
+        /// </summary>
+        /// <param name="id">The id of the time zone to return.</param>
+        /// <returns>
+        /// The <see cref="DateTimeZone"/> or null if there is no time zone with the given id.
+        /// </returns>
+        public DateTimeZone ForId(string id)
+        {
+            string canonicalId;
+            if (!timeZoneIdMap.TryGetValue(id, out canonicalId))
+            {
+                throw new ArgumentException("Time zone with ID " + id + " not found in source " + version, "id");
+            }
+            return ResourceHelper.LoadTimeZone(source, canonicalId, id);
+        }
+
+        /// <summary>
+        /// Returns a sequence of the available IDs from this source.
+        /// </summary>
+        [DebuggerStepThrough]
+        public IEnumerable<string> GetIds()
+        {
+            return timeZoneIdMap.Keys;
+        }
+
+        /// <summary>
+        /// Returns a version identifier for this source.
+        /// </summary>
+        public string VersionId { get { return "TZDB: " + version; } }
+
+        /// <summary>
+        /// Attempts to map the system time zone to a zoneinfo ID, and return that ID.
+        /// </summary>
+        public string MapTimeZoneId(TimeZoneInfo zone)
+        {
+#if PCL
+            throw new NotSupportedException();
+#else
+            string result;
+            windowsIdMap.TryGetValue(zone.Id, out result);
+            return result;
+#endif
+        }
+
+        /// <summary>
+        /// Returns a lookup from canonical ID (e.g. "Europe/London") to a group of aliases
+        /// (e.g. {"Europe/Belfast", "Europe/Guernsey", "Europe/Jersey", "Europe/Isle_of_Man", "GB", "GB-Eire"}).
+        /// </summary>
+        /// <remarks>
+        /// The group of values for a key never contains the canonical ID, only aliases. Any time zone
+        /// ID which is itself an alias or has no aliases linking to it will not be present in the lookup.
+        /// The aliases within a group are returned in alphabetical (ordinal) order.
+        /// </remarks>
+        /// <returns>A lookup from canonical ID to the aliases of that ID.</returns>
+        public ILookup<string, string> Aliases { get { return aliases; } }
+
+        /// <summary>
+        /// Returns a read-only map from time zone ID to the canonical ID. For example, the key "Europe/Jersey"
+        /// would be associated with the value "Europe/London".
+        /// </summary>
+        /// <remarks>
+        /// <para>This map contains an entry for every ID returned by <see cref="GetIds"/>, where
+        /// canonical IDs map to themselves.</para>
+        /// <para>The returned map is read-only; any attempts to call a mutating method will throw
+        /// <see cref="NotSupportedException" />.</para>
+        /// </remarks>
+        /// <returns>A map from time zone ID to the canonical ID.</returns>
+        public IDictionary<string, string> CanonicalIdMap { get { return timeZoneIdMap; } }
+    }
+}
---
+++ src/NodaTime/Utility/ResourceHelper.cs
@@ -0,0 +1,170 @@
+#region Copyright and license information
+// Copyright 2001-2009 Stephen Colebourne
+// Copyright 2009-2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Resources;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NodaTime.Properties;
+using NodaTime.TimeZones;
+
+namespace NodaTime.Utility
+{
+    /// <summary>
+    /// Provides helper methods for using resources.
+    /// </summary>
+    internal static class ResourceHelper
+    {
+#if PCL
+        private static readonly Regex InvalidResourceNameCharacters = new Regex("[^A-Za-z0-9_/]", RegexOptions.CultureInvariant);
+#else
+        private static readonly Regex InvalidResourceNameCharacters = new Regex("[^A-Za-z0-9_/]", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+#endif
+
+        internal static CultureInfo GetCulture(IFormatProvider formatProvider)
+        {
+            return formatProvider as CultureInfo ?? Thread.CurrentThread.CurrentUICulture;
+        }
+
+        /// <summary>
+        /// Returns the message string from the package resources formatted with the given replacement parameters.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// If there is no resource string with the given id then the id is returned as the message. 
+        /// </para>
+        /// </remarks>
+        /// <param name="id">the message id to retrieve.</param>
+        /// <param name="replacements">The optional replacement parameters.</param>
+        /// <returns>The formatted string.</returns>
+        internal static string GetMessage(string id, params object[] replacements)
+        {
+            var message = Messages.ResourceManager.GetString(id) ?? id;
+            return String.Format(CultureInfo.CurrentCulture, message, replacements);
+        }
+
+        /// <summary>
+        /// Returns the message string from the package resources formatted with the given replacement parameters.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// If there is no resource string with the given id then the id is returned as the message. 
+        /// </para>
+        /// </remarks>
+        /// <param name="formatProvider"></param>
+        /// <param name="invariant"></param>
+        /// <param name="id">the message id to retrieve.</param>
+        /// <param name="replacements">The optional replacement parameters.</param>
+        /// <returns>The formatted string.</returns>
+        internal static string GetMessage(IFormatProvider formatProvider, string invariant, string id, params object[] replacements)
+        {
+            var culture = GetCulture(formatProvider);
+            string message = culture.Equals(CultureInfo.InvariantCulture) ? invariant : (Messages.ResourceManager.GetString(id) ?? id);
+            return replacements.Length == 0 ? message : String.Format(CultureInfo.CurrentCulture, message, replacements);
+        }
+
+        /// <summary>
+        /// Normalizes the given name into a valid resource name by replacing invalid
+        /// characters with alternatives.
+        /// </summary>
+        /// <param name="name">The name to normalize.</param>
+        /// <returns>The normalized name.</returns>
+        internal static string NormalizeAsResourceName(string name)
+        {
+            Preconditions.CheckNotNull(name, "name");
+            name = name.Replace("-", "_minus_");
+            name = name.Replace("+", "_plus_");
+            name = name.Replace("<", "_less_");
+            name = name.Replace(">", "_greater_");
+            name = name.Replace("&", "_and_");
+            return InvalidResourceNameCharacters.Replace(name, "_");
+        }
+
+#if !PCL
+        /// <summary>
+        /// Gets the default <see cref="IResourceSet"/> from a <see cref="ResourceManager"/>.
+        /// </summary>
+        /// <param name="manager">The <see cref="ResourceManager"/> to get resources from.</param>
+        /// <returns>The default <see cref="IResourceSet"/>.</returns>
+        /// <remarks>The default <see cref="IResourceSet"/> for a <see cref="ResourceManager"/> is
+        /// the <see cref="IResourceSet"/> that is used by <see cref="ResourceManager.GetObject(string)"/>.</remarks>
+        internal static IResourceSet GetDefaultResourceSet(ResourceManager manager)
+        {
+            return new BclResourceSet(manager.GetResourceSet(CultureInfo.CurrentUICulture, true, true));
+        }
+#endif
+
+        internal static IResourceSet GetDefaultResourceSet(string baseName, Assembly assembly)
+        {
+#if PCL
+            using (Stream stream = assembly.GetManifestResourceStream(baseName + ".nodaresources"))
+            {
+                return NodaResourcesResourceSet.FromStream(stream);
+            }
+#else
+            return GetDefaultResourceSet(new ResourceManager(baseName, assembly));
+#endif
+        }
+
+        /// <summary>
+        /// Loads a dictionary of string to string with the given name from the given resource manager.
+        /// </summary>
+        /// <param name="source">The <see cref="IResourceSet"/> to load from.</param>
+        /// <param name="name">The resource name.</param>
+        /// <returns>The <see cref="IDictionary{TKey,TValue}"/> or null if there is no such resource.</returns>
+        internal static IDictionary<string, string> LoadDictionary(IResourceSet source, string name)
+        {
+            Preconditions.CheckNotNull(source, "source");
+            var bytes = source.GetObject(name) as byte[];
+            if (bytes != null)
+            {
+                using (var stream = new MemoryStream(bytes))
+                {
+                    var reader = new DateTimeZoneReader(stream);
+                    return reader.ReadDictionary();
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Loads a time zone with the given name from the given resource manager.
+        /// </summary>
+        /// <param name="source">The <see cref="IResourceSet"/> to load from.</param>
+        /// <param name="name">The resource name. (This will not be normalized.)</param>
+        /// <param name="id">The time zone id for the loaded time zone.</param>
+        /// <returns>The <see cref="DateTimeZone"/> parsed from the resources.</returns>
+        /// <exception cref="ArgumentException">The </exception>
+        internal static DateTimeZone LoadTimeZone(IResourceSet source, string name, string id)
+        {
+            Preconditions.CheckNotNull(source, "source");
+            object obj = source.GetObject(NormalizeAsResourceName(name));
+            // We should never be asked for time zones which don't exist.
+            Preconditions.CheckArgument(obj != null, "id", "ID is not one of the recognized time zone identifiers within this resource");
+            byte[] bytes = (byte[])obj;
+            using (var stream = new MemoryStream(bytes))
+            {
+                var reader = new DateTimeZoneReader(stream);
+                return reader.ReadTimeZone(id);
+            }
+        }
+    }
+}
