0f804250a58b88fbd34725eb6ff34ff33ebc0c68
Jon Skeet <skeet@pobox.com>
2014-06-27 11:13:08 +0100

    Add 1.3.0 XML documentation (so we can tell what's new in 2.0).

---
+++ src/NodaTime/Fields/FixedLengthDatePeriodField.cs
@@ -0,0 +1,40 @@
+﻿// Copyright 2014 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+namespace NodaTime.Fields
+{
+    /// <summary>
+    /// Date period field for fixed-length periods (weeks and days).
+    /// </summary>
+    internal sealed class FixedLengthDatePeriodField : IDatePeriodField
+    {
+        private readonly int unitDays;
+
+        internal FixedLengthDatePeriodField(int unitDays)
+        {
+            this.unitDays = unitDays;
+        }
+
+        public LocalDate Add(LocalDate localDate, int value)
+        {
+            if (value == 0)
+            {
+                return localDate;
+            }
+            int days = localDate.DaysSinceEpoch + value * unitDays;
+            return new LocalDate(days, localDate.Calendar);
+        }
+
+        public int Subtract(LocalDate minuendDate, LocalDate subtrahendDate)
+        {
+            if (minuendDate == subtrahendDate)
+            {
+                return 0;
+            }
+            int minuendDays = minuendDate.DaysSinceEpoch;
+            int subtrahendDays = subtrahendDate.DaysSinceEpoch;
+            return (minuendDays - subtrahendDays) / unitDays;
+        }
+    }
+}
