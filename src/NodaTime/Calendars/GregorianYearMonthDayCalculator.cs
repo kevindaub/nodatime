0e85449dcbe6ccbadc0e2c38746114c2ab8f7de1
Malcolm Rowe <malcolm.rowe@gmail.com>
2014-06-28 19:21:50 +0100

    Stop setting a sticky-session cookie on every browser.
    (This is a reapplication of r1507d3cc9d85 to the Web.config location that
    we're actually using now.)

---
+++ src/NodaTime/Calendars/GregorianYearMonthDayCalculator.cs
@@ -0,0 +1,104 @@
+﻿// Copyright 2013 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using System;
+
+namespace NodaTime.Calendars
+{
+    internal class GregorianYearMonthDayCalculator : GJYearMonthDayCalculator
+    {
+        // We precompute useful values for each month between these years, as we anticipate most
+        // dates will be in this range.
+        private const int FirstOptimizedYear = 1900;
+        private const int LastOptimizedYear = 2100;
+        private static readonly int[] MonthStartDays = new int[(LastOptimizedYear + 1 - FirstOptimizedYear) * 12 + 1];
+        private static readonly int[] MonthLengths = new int[(LastOptimizedYear + 1 - FirstOptimizedYear) * 12 + 1];
+        private static readonly int[] YearStartDays = new int[LastOptimizedYear + 1 - FirstOptimizedYear];
+
+        private const int DaysFrom0000To1970 = 719527;
+        private const int AverageDaysPer10Years = 3652; // Ideally 365.2425 per year...
+
+        static GregorianYearMonthDayCalculator()
+        {
+            // It's generally a really bad idea to create an instance before the static initializer
+            // has completed, but we know its safe because we're only using a very restricted set of methods.
+            var instance = new GregorianYearMonthDayCalculator();
+            for (int year = FirstOptimizedYear; year <= LastOptimizedYear; year++)
+            {
+                int yearStart = instance.CalculateStartOfYearDays(year);
+                YearStartDays[year - FirstOptimizedYear] = yearStart;
+                int monthStartDay = yearStart;
+                int yearMonthIndex = (year - FirstOptimizedYear) * 12;
+                for (int month = 1; month <= 12; month++)
+                {
+                    yearMonthIndex++;
+                    int monthLength = instance.GetDaysInMonth(year, month);
+                    MonthStartDays[yearMonthIndex] = monthStartDay;
+                    MonthLengths[yearMonthIndex] = monthLength;
+                    monthStartDay += monthLength;
+                }
+            }
+        }
+
+        internal GregorianYearMonthDayCalculator()
+            : base(-27255, 31195, AverageDaysPer10Years, -719162)
+        {
+        }
+
+        // TODO(2.0): Check that this is worth doing, given our normal cache.
+        internal override int GetStartOfYearInDays(int year)
+        {
+            if (year < FirstOptimizedYear || year > LastOptimizedYear)
+            {
+                return base.GetStartOfYearInDays(year);
+            }
+            return YearStartDays[year - FirstOptimizedYear];
+        }
+
+        internal override int GetDaysSinceEpoch(YearMonthDay yearMonthDay)
+        {
+            int year = yearMonthDay.Year;
+            int monthOfYear = yearMonthDay.Month;
+            int dayOfMonth = yearMonthDay.Day;
+            int yearMonthIndex = (year - FirstOptimizedYear) * 12 + monthOfYear;
+            if (year < FirstOptimizedYear || year > LastOptimizedYear - 1 || monthOfYear < 1 || monthOfYear > 12 || dayOfMonth < 1 ||
+                dayOfMonth > MonthLengths[yearMonthIndex])
+            {
+                return base.GetDaysSinceEpoch(yearMonthDay);
+            }
+            // This is guaranteed not to overflow, as we've already validated the arguments
+            return unchecked(MonthStartDays[yearMonthIndex] + (dayOfMonth - 1));
+        }
+
+        protected override int CalculateStartOfYearDays(int year)
+        {
+            // Initial value is just temporary.
+            int leapYears = year / 100;
+            if (year < 0)
+            {
+                // Add 3 before shifting right since /4 and >>2 behave differently
+                // on negative numbers. When the expression is written as
+                // (year / 4) - (year / 100) + (year / 400),
+                // it works for both positive and negative values, except this optimization
+                // eliminates two divisions.
+                leapYears = ((year + 3) >> 2) - leapYears + ((leapYears + 3) >> 2) - 1;
+            }
+            else
+            {
+                leapYears = (year >> 2) - leapYears + (leapYears >> 2);
+                if (IsLeapYear(year))
+                {
+                    leapYears--;
+                }
+            }
+
+            return year * 365 + (leapYears - DaysFrom0000To1970);
+        }
+
+        internal override bool IsLeapYear(int year)
+        {
+            return ((year & 3) == 0) && ((year % 100) != 0 || (year % 400) == 0);
+        }
+    }
+}
---
+++ src/NodaTime/Fields/FixedLengthDatePeriodField.cs
@@ -0,0 +1,58 @@
+﻿// Copyright 2014 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using NodaTime.Calendars;
+
+namespace NodaTime.Fields
+{
+    /// <summary>
+    /// Date period field for fixed-length periods (weeks and days).
+    /// </summary>
+    internal sealed class FixedLengthDatePeriodField : IDatePeriodField
+    {
+        private readonly int unitDays;
+
+        internal FixedLengthDatePeriodField(int unitDays)
+        {
+            this.unitDays = unitDays;
+        }
+
+        public LocalDate Add(LocalDate localDate, int value)
+        {
+            if (value == 0)
+            {
+                return localDate;
+            }
+            int daysToAdd = value * unitDays;
+            if (daysToAdd < 20 && daysToAdd > -20)
+            {
+                YearMonthDayCalculator calculator = localDate.Calendar.YearMonthDayCalculator;
+                YearMonthDay yearMonthDay = localDate.YearMonthDay;
+                int year = yearMonthDay.Year;
+                int month = yearMonthDay.Month;
+                int day = yearMonthDay.Day;
+                int newDayOfMonth = day + daysToAdd;
+                if (1 <= newDayOfMonth && newDayOfMonth <= calculator.GetDaysInMonth(year, month))
+                {
+                    return new LocalDate(new YearMonthDay(year, month, newDayOfMonth), localDate.Calendar);
+                }
+                // TODO(2.0): Improve this if we're still in the same year. Would be reasonably simple
+                // if we had GetYearMonthDay(year, dayOfYear).
+            }
+            int days = localDate.DaysSinceEpoch + daysToAdd;
+            return new LocalDate(days, localDate.Calendar);
+        }
+
+        public int Subtract(LocalDate minuendDate, LocalDate subtrahendDate)
+        {
+            if (minuendDate == subtrahendDate)
+            {
+                return 0;
+            }
+            int minuendDays = minuendDate.DaysSinceEpoch;
+            int subtrahendDays = subtrahendDate.DaysSinceEpoch;
+            return (minuendDays - subtrahendDays) / unitDays;
+        }
+    }
+}
