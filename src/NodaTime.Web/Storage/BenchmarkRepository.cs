6595590fc35c331168325a762f5cc00a9f1ed6bc
Malcolm Rowe <malcolm.rowe@gmail.com>
2014-07-01 13:26:41 +0100

    Add a link to the NZD download page.

---
+++ src/NodaTime.Web/Storage/BenchmarkRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace NodaTime.Web.Storage
+{
+    /// <summary>
+    /// Storage for a set of benchmark runs.
+    /// </summary>
+    public class BenchmarkRepository
+    {
+        public ImmutableList<BenchmarkRun> AllRuns { get; private set;  }
+        public ILookup<string, BenchmarkRun> RunsByMachine { get; private set; }
+        public TimeSpan LoadingTime { get; private set; }
+
+        private BenchmarkRepository(ImmutableList<BenchmarkRun> allRuns, TimeSpan loadingTime)
+        {
+            AllRuns = allRuns;
+            RunsByMachine = allRuns.ToLookup(x => x.Machine);
+            LoadingTime = loadingTime;
+        }
+
+        internal BenchmarkRun GetRun(string machine, string label)
+        {
+            return RunsByMachine[machine].FirstOrDefault(f => f.Label == label);
+        }
+
+        internal static BenchmarkRepository Load(string directory)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            string index = Path.Combine(directory, "index.txt");
+            var files = File.ReadLines(index)
+                .Select(file => XDocument.Load(Path.Combine(directory, file)))
+                .Select(BenchmarkRun.FromXDocument)
+                .OrderByDescending(b => b.StartTime)
+                .ToImmutableList();
+            sw.Stop();
+            return new BenchmarkRepository(files, sw.Elapsed);
+        }
+
+        public static string HashForLabel(string label)
+        {
+            return label.Split('.')[0];
+        }
+    }
+}
